c054ab467c8b6aef31ccbd91e093816ea03d2541
unknown <eoghanf34@gmail.com>
2023-04-17 09:55:06 +0000

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-stories:any[]=[];
-  constructor() {}
+export class HomePage  {
+myStatus: string = "";
+  constructor(private storage: Storage) {}
   
-}
+
+  
+  ionViewWillEnter(){
+    this.storage.create().then(
+      () => {
+      this.storage.get('status')
+      .then((data) => {
+      this.myStatus = data;
+      })
+      .catch();
+      })
+      .catch();
+      }
+  }
+
